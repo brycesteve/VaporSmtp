80e585363d7e7f3a957f30df915a74cb20540a89
Steve Bryce <steve@stevebryce.net>
2020-02-17 10:24:48 +0000

    Refactor

--- Package.swift
+++ Package.swift
@@ -1,20 +1,26 @@
 // swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "VaporSmtp",
-    platforms: [
-        // specify each minimum deployment requirement,
-        //otherwise the platform default minimum is used.
-       .macOS(.v10_14),
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "VaporSmtp",
+            targets: ["VaporSmtp"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.3"),
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+	.package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.3"),   
     ],
     targets: [
-        .target(name: "VaporSmtp", dependencies: [
-            .product(name: "Vapor", package: "vapor"),
-        ])
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "VaporSmtp",
+            dependencies: [.product(name: "Vapor", package: "vapor")]),
     ]
 )
-
