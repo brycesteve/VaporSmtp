129b2238f45318f50322871eb69c296473115518
Steve Bryce <steve@stevebryce.net>
2020-02-17 12:22:01 +0000

    Add platform version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "VaporSmtp",
+    platforms: [
+        .macOS(.v10_14)
+    ],
     products: [
         .library(
             name: "VaporSmtp",
