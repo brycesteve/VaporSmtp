0639123525d04b13fde76deca49236414654bcbf
Steve Bryce <steve@stevebryce.net>
2020-02-17 12:13:27 +0000

    Minor changes

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "VaporSmtp",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "VaporSmtp",
             targets: ["VaporSmtp"]),
@@ -14,13 +13,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-	.package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.3"),   
+	    .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.3"),   
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "VaporSmtp",
-            dependencies: [.product(name: "Vapor", package: "vapor")]),
+            dependencies: [
+                .product(name: "Vapor", package: "vapor")
+            ]
+        ),
     ]
 )
